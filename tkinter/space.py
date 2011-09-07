c33fc0aa10fbc11086d763ee51f6c0f416553de2
Bruno Gola <brunogola@gmail.com>
2011-09-07 17:55:53 -0300

    major refactor

--- tkinter/space.py
+++ tkinter/space.py
@@ -8,57 +8,56 @@ canvas = Canvas(master, width=800, height=600, background='black')
 canvas.pack()
 
 class Grid(object):
+    cell_size = 5
+
     def __init__(self, canvas):
+        self.columns = int(canvas['width']) / self.cell_size
+        self.rows = int(canvas['height']) / self.cell_size
         self.canvas = canvas
-        self.cell_width = 10
-        self.cell_height = 10
-        self.width = 80
-        self.height = 60
 
     def draw(self, x, y, tags=None):
-        self.canvas.create_rectangle(x * self.cell_width,
-                                     y*self.cell_height,
-                                     x * self.cell_width + self.cell_width,
-                                     y*self.cell_height + self.cell_height, 
+        self.canvas.create_rectangle(x * self.cell_size,
+                                     y*self.cell_size,
+                                     x * self.cell_size + self.cell_size,
+                                     y*self.cell_size + self.cell_size, 
                                      fill="white", outline="white", tags=tags)
 
-    def draw_object(self, x, y, obj):
-        for j, line in enumerate(obj):
-            for i, v in enumerate(line):
-                if v:
-                    self.draw(x+i, y+j, obj.tags)
+class Unit(object):
+    bitmap = None
+    instance_count = 0
 
-class Object(object):
-    def __init__(self, obj, x=None, y=None, tags=None):
-        self.__obj = obj
+    def __init__(self, grid, x=0, y=0, tags=()):
         self.x = x
         self.y = y
-        self.tags = tags
+        self.grid = grid
+        self.tags = (self.__class__.__name__, self.__class__.instance_count) + tags
+        self.__class__.instance_count += 1
         
-    def __iter__(self):
-        return self.__obj.__iter__()
-
     def draw(self):
-        canvas.draw_object(self)
-
-grid = Grid(canvas)
-
-spaceship = Object([[0,0,1,0,0],
-                    [1,0,1,0,1],
-                    [1,1,1,1,1]], tags="spaceship")
+        for j, line in enumerate(self.bitmap):
+            for i, v in enumerate(line):
+                if v:
+                    self.grid.draw(self.x+i, self.y+j, self.tags)
 
 
-heart = Object([[0,1,0,1,0],
-                [1,1,1,1,1],
-                [1,1,1,1,1],
-                [0,1,1,1,0],
-                [0,0,1,0,0]])
+def testcase():
+    grid = Grid(canvas)
 
-bullet = [[1]]
-import random
-grid.draw_object(37, 55, spaceship)
-for i, x in enumerate(range(10, 65, 10)):
-    grid.draw_object(x, random.randint(1,10), Object([[1,0,1],
-                                                      [0,1,0]], tags=("enemy%d" %i, "enemies")))
+    class Spaceship(Unit):
+        bitmap =((0,0,1,0,0),
+                 (1,0,1,0,1),
+                 (1,1,1,1,1))
+
+    class Enemy(Unit):
+        bitmap = ((1,0,1),
+                  (0,1,0))
+
+    import random
+    sp = Spaceship(grid, 80, 100)
+    sp.draw()
+    for i, x in enumerate(range(10, 65, 5)):
+        enemy = Enemy(grid, x, random.randint(1,10))
+        enemy.draw()
 #mainloop()
+testcase()
 
